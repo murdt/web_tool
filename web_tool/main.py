909654de32028aa2fc95384d7018958e68c2ebb3
Yakup Murad Bizsan <ymbizsan@outlook.com>
2020-07-03 10:39:45 +0300

    added maintenance menu

--- web_tool/main.py
+++ web_tool/main.py
@@ -1,15 +1,21 @@
-from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request,jsonify
 from wtforms import Form,StringField,PasswordField,TextAreaField,validators
 from flask_sqlalchemy import SQLAlchemy
 import urllib
 from functools import wraps
 import pyodbc
-import dimod
 from flask_table import Table,Col,LinkCol
+from datetime import datetime
+import requests
+import json
+
 
 app = Flask(__name__)
 
 app.secret_key = "web_tool"
+
+class MaintainerForm(Form):
+    name = StringField("İsim")
 
 
 class UpdateForm(Form):
@@ -17,6 +23,18 @@ class UpdateForm(Form):
     last_name = StringField("Soyisim")
     username = StringField("Kullanıcı Adı")
     password = StringField("Şifre")
+
+
+class UpdateMaintenceForm(Form):
+    date = StringField("Arızanın Meydana Geldiği Tarih:")
+    last_date = StringField("Arızanın Meydana Giderildiği Tarih:")
+    totalhour = StringField("Toplam Duruş:")
+    section = StringField("Arızanın Meydana Geldiği Bölüm:")
+    person = StringField("Arızayı Gideren Kişi:")
+    description = StringField("Arızanın Tanımı:")
+    status = StringField("Durum:")
+    operation = StringField("Yapılan İşlem:")
+
 
 class RegisterForm(Form):
     name = StringField("İsim")
@@ -40,6 +58,8 @@ class LoginForm(Form):
     password = PasswordField("Password")
 
 conn = pyodbc.connect("DRIVER={SQL Server};SERVER=VEEAMBACKUP\VEEAMSQL2016;PORT=1433;DATABASE=web_server;UID=sa;PWD=yasinc;")
+conn2 = pyodbc.connect("DRIVER={SQL Server};SERVER=VEEAMBACKUP\VEEAMSQL2016;PORT=1433;DATABASE=web_server;UID=sa;PWD=yasinc;")
+cursor2 = conn2.cursor()
 cursor = conn.cursor()
 
 @app.route("/")
@@ -174,10 +194,103 @@ def updateuser(id):
     else:
         return redirect(url_for("index"))
 @app.route("/otomaticorder")
+@login_required
 def order():
     form = OrderForm(request.form)
     return render_template("otomaticorder.html", form = form)
-    
+
+@app.route("/maintenance")
+@login_required
+def maintenancemain():
+        total = "" 
+        cursor.execute("SELECT * FROM maintenance")
+        data = cursor.fetchall()
+        cursor2.execute("SELECT name,maintenanceid FROM maintainers")
+        data2 = cursor2.fetchall()
+        while data2[1] == data[0]:
+            total = total + " " + data2[0]
+        return render_template("maintenance/maintenancemain.html",value=data, value2=data2, bruh=total)
+
+@app.route("/addmaintenanceform", methods = ["GET", "POST"])
+@login_required
+def maintenanceadd():
+    form = UpdateMaintenceForm(request.form)
+    if request.method == "GET":
+        return render_template("maintenance/addmaintenance.html",form = form)  
+    else:
+        datestring = str(form.date.data)
+        last_datestring = str(form.last_date.data)
+        date  = datetime.strptime(datestring,'%d-%m-%Y')
+        last_date  = datetime.strptime(last_datestring,'%d-%m-%Y')
+        totalhour = form.totalhour.data
+        section = form.section.data
+        description = form.description.data
+        status = form.status.data
+        operation = form.operation.data
+        cursor = conn.cursor()
+        query = "INSERT INTO maintenance (date,last_date,totalhour,section,description,status,operation) VALUES (?,?,?,?,?,?,?,?)"
+        cursor.execute(query,[date,last_date,totalhour,section,description,status,operation])
+        cursor.commit()
+        return redirect(url_for("maintenancemain"))
+
+@app.route("/deletemaintenanceform/<string:id>")
+@login_required
+def deletemaintenanceform(id):  
+    cursor = conn.cursor()
+    query = "DELETE FROM maintenance WHERE id=?"
+    cursor.execute(query,[id])
+    cursor.commit()
+    return redirect(url_for("maintenancemain"))
+
+
+@app.route("/updatemaintenanceform/<string:id>", methods = ["GET","POST"])
+@login_required
+def updatemaintenanceform(id):
+    form = UpdateMaintenceForm(request.form)
+    if request.method == "GET":
+        cursor = conn.cursor()
+        query = "SELECT * FROM maintenance WHERE id =?"
+        cursor.execute(query,[id])
+        info = cursor.fetchone()
+        form.date.data = info[1]
+        form.last_date.data = info[2]
+        form.totalhour.data = info[3]
+        form.section.data = info[4]
+        form.person.data = info[5]
+        form.description.data = info[6]
+        form.status.data = info[7]
+        form.operation.data = info[8]
+        return render_template("maintenance/maintenanceupdate.html", form = form)
+    else:
+        date = form.date.data
+        last_date = form.last_date.data
+        totalhour = form.totalhour.data
+        section = form.section.data
+        person = form.person.data
+        description = form.description.data
+        status = form.status.data
+        operation = form.operation.data
+        cursor = conn.cursor()
+        query = "UPDATE maintenance SET date=?,last_date=?,totalhour=?,section=?,person=?,description=?,status=?,operation=? WHERE id=?"
+        cursor.execute(query,[date,last_date,totalhour,section,person,description,status,operation,id])
+        return redirect(url_for("maintenancemain"))
+
+@app.route("/addmaintainer/<string:id>", methods = ["GET", "POST"])
+@login_required
+def addmaintainer(id):
+    form = MaintainerForm(request.form)
+    if request.method == "GET":
+        cursor = conn.cursor()
+        cursor.execute("SELECT id FROM maintenance")
+        data = cursor.fetchall() 
+        return render_template("maintenance/addmaintainer.html",value=data, form=form)
+    else:
+        cursor = conn.cursor()
+        name = form.name.data
+        query = "INSERT INTO maintainers (name,maintenanceid) VALUES (?,?)"
+        cursor.execute(query,[name,id])
+        cursor.commit()
+        return redirect(url_for("maintenancemain"))
 
 
 if __name__ == "__main__":
